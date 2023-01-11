65d21da5746ba5a13207d85dd7b2448387db86a4
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-01-11 10:36:24 -0800

    refactor

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return (list(map(lambda x: x*number, my_list)))
+def multiply_list_map(my_list=[], number=0):
+    x_list = my_list
+    return (list(map(lambda x: x*number, x_list)))
