0d91dae71aa0ea3b024001bc2c2d139744dc5dad
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-01-25 04:07:07 -0800

    refactored

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
-"""Define a class, Square."""
-
 class Square:
-    """Represent a square"""
+    """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square.
+        """Initialize a new Square.
         Args:
-            size(int): The size of the new square.
+            size (int): The size of the new square.
         """
-        self._size = size
+        self.__size = size
