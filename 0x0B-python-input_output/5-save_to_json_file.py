a003d14663a20acb5a40288ebf13304ddebf87bf
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-02-08 07:47:07 -0800

    Chore: refactored

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Defines a string-to-JSON function."""
+"""Defines a JSON file-writing function."""
 import json
 
 
-def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
-    return json.dumps(my_obj)
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
