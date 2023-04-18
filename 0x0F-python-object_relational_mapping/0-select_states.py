7b2c1e6f571d1d837fe126df9f2a7196791bb0f3
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-04-18 04:04:09 -0700

    rfac

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Lists all unique states from the database hbtn_0e_0_usa
+This module lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
 
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=db_name)
-
-    cursor = db.cursor()
-    cursor.execute("SELECT DISTINCT id, name FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-
+def list_all_states(username, password, database):
+    """
+    Lists all states from the database hbtn_0e_0_usa
+    """
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    conn.close()
 
-    cursor.close()
-    db.close()
 
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        list_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        print("Usage: {} username password database".format(sys.argv[0]))
