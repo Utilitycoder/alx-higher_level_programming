d35613e362543f6c5c8180b01f16f40bc1b73a22
Lawal Abubakar Babatunde <utility4all@gmail.com>
2023-04-18 02:58:44 -0700

    refac

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -20,10 +20,10 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
 
-    for row in rows:
+    for row in cursor:
         print(row)
 
     cursor.close()
     db.close()
+
